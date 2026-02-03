179c76207b461aa035c6989299a0fa30938cf859
Chasel <arko9988b@gmail.com>
2026-02-03 17:27:58 +0800

    faet: implement counter feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+import { useState } from 'react';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const disableButtonText = isDisabled ? 'ABLE' : 'DISABLE'
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleClear = () => {
+    setCount(0);
+  };
+
+  const handleToggleDisable = () => {
+    setIsDisabled((prev) => !prev);
+  };
+
   return (
-    <></>
-  )
+    <ButtonGroup
+      orientation="vertical"
+      variant="outlined"
+      sx={(theme) => ({
+        '& .MuiButtonGroup-grouped': {
+          fontWeight: 'bold',
+        },
+        '& .MuiButtonGroup-firstButton': {
+          ...(isDisabled && {
+            pointerEvents: 'none',
+            opacity: theme.palette.action.disabledOpacity,
+            color: theme.palette.action.disabled,
+            backgroundColor: theme.palette.action.disabledBackground,
+          })
+        },
+      })}
+      >
+      <Button onClick={handleClick}>
+        CLICK:{count}
+      </Button>
+      <Button onClick={handleClear}>CLEAR</Button>
+      <Button onClick={handleToggleDisable}>
+        {disableButtonText}
+      </Button>
+    </ButtonGroup>
+  );
 }
 
-export default App
+export default App;
